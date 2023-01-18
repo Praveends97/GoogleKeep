75d33282991d389dbc644a1dd8a49549279620bd
Praveen <Praveen@DESKTOP-H1O9U8H>
2023-01-18 11:49:34 +0530

    Add project files.

---
+++ Data/GooglekeppContext.cs
@@ -0,0 +1,17 @@
+﻿using GoogleKeep.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+namespace GoogleKeep.Data
+{
+    public class GooglekeppContext : DbContext
+    {
+        public GooglekeppContext(DbContextOptions<GooglekeppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Googlekeep> Googlekeep { get; set; }
+    }
+}
+
---
+++ Migrations/20230118053334_AddGooglekeepToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoogleKeep.Migrations
+{
+    public partial class AddGooglekeepToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Googlekeep",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Googlekeep", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Googlekeep");
+        }
+    }
+}
